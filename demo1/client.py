eda63b7074bb39a65b7c342bae726757b9236556
Philip Lombardi <plombardi@datawire.io>
2016-10-21 11:39:50 -0400

    close on keyboardinterrupt

--- demo1/client.py
+++ demo1/client.py
@@ -13,6 +13,8 @@ try:
         try:
             print requests.get(ssn.resolve("hello", "1.0").address).text
             time.sleep(1.0)
+        except KeyboardInterrupt:
+            sys.exit(0)
         except:
             pass
 except KeyboardInterrupt:
